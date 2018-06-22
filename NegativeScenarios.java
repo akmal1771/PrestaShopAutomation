3825e9ba0e904ccea1b469e7c124102fda0d23c2
akmal <akmalomonov11@gmail.com>
2018-06-21 21:39:18 -0500

    adding 2 scenarios

---
+++ NegativeScenarios.java
@@ -0,0 +1,80 @@
+package com.prestashop.tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class NegativeScenarios {
+	
+	WebDriver dr;
+	@BeforeClass
+	public void setup() {
+		WebDriverManager.chromedriver().setup();
+		dr=new ChromeDriver();
+	}
+	
+	
+	@Test
+	public void wrongCredentialsTest() {
+		dr.get("http://automationpractice.com/index.php");	
+		dr.findElement(By.className("login")).click();
+		
+	   String email="akmalomonov@yahoo.com";
+		dr.findElement(By.id("email")).sendKeys(email);
+	   String password="2145akmal";	
+		dr.findElement(By.id("passwd")).sendKeys(password);
+		WebElement signIn=dr.findElement(By.xpath("//button[@id='SubmitLogin']"));
+		signIn.click();
+		String expectedNegativeMess="Authentication failed.";
+		String error=dr.findElement(By.xpath("//div[@class='alert alert-danger']/ol/li")).getText();
+		
+	    Assert.assertEquals(expectedNegativeMess,error,"Authentication failed.");
+	}
+	@Test
+	public void invalidEmailTest() {
+		dr.get("http://automationpractice.com/index.php");	
+		dr.findElement(By.className("login")).click();
+		
+		dr.findElement(By.id("email")).sendKeys("akmla");
+		dr.findElement(By.id("passwd")).sendKeys("2145akmal");
+		WebElement signIn=dr.findElement(By.xpath("//button[@id='SubmitLogin']"));
+		signIn.click();
+		String expectedResult="Invalid email address.";
+		String result=dr.findElement(By.xpath("//div[@class='alert alert-danger']/ol/li")).getText();
+	   // System.out.println(error);
+		Assert.assertEquals(expectedResult, result);
+	
+	}
+	@Test 
+	public void blankEmailTest() {
+		dr.get("http://automationpractice.com/index.php");	
+		dr.findElement(By.className("login")).click();
+		
+		dr.findElement(By.id("passwd")).sendKeys("akmaliddin");
+		WebElement signIn=dr.findElement(By.xpath("//button[@id='SubmitLogin']"));
+		signIn.click();
+		String expectedMessage="An email address required.";
+		String blankError=dr.findElement(By.xpath("//div[@class='alert alert-danger']/ol/li")).getText();
+		Assert.assertEquals(expectedMessage, blankError);
+	}
+
+	@Test 
+	public void blankPasswordTest() {
+		dr.get("http://automationpractice.com/index.php");	
+		dr.findElement(By.className("login")).click();
+		
+		dr.findElement(By.id("email")).sendKeys("akmalomonov@yahoo.com");
+		dr.findElement(By.xpath("//button[@id='SubmitLogin']")).click();
+		String message="Password is required.";
+		String blankPssw=dr.findElement(By.xpath("//div[@class='alert alert-danger']/ol/li")).getText();
+		Assert.assertEquals(message, blankPssw);
+		
+	}
+}
---
+++ PositiveScenarios.java
@@ -0,0 +1,76 @@
+package com.prestashop.tests;
+
+import java.util.Random;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class PositiveScenarios {
+
+	
+		
+		WebDriver dr;
+		@BeforeClass
+		public void setup() {
+			WebDriverManager.chromedriver().setup();
+			dr=new ChromeDriver();
+			
+		}
+		
+		@Test
+		public void loginTest() throws InterruptedException {
+			dr.get("http://automationpractice.com/index.php");	
+			dr.findElement(By.className("login")).click();
+			Random ran=new Random();
+			int r=ran.nextInt(10);
+			dr.findElement(By.id("email_create")).sendKeys("akmal"+r+"@yahoo.com");
+			dr.findElement(By.id("SubmitCreate")).click();
+			Thread.sleep(3000);
+			dr.findElement(By.xpath("//input[@id='id_gender1']")).click();
+			dr.findElement(By.xpath("//input[@id='id_gender1']")).click();
+			//Personal info
+			dr.findElement(By.xpath("//input[@name='customer_firstname']")).sendKeys("akmal");
+			dr.findElement(By.xpath("//input[@name='customer_lastname']")).sendKeys("mabruka");
+			dr.findElement(By.xpath("//input[@name='passwd']")).sendKeys("12365478");
+			dr.findElement(By.xpath("//select[@id='days']")).sendKeys("14");
+			dr.findElement(By.xpath("//select[@id='months']")).sendKeys("June");
+			dr.findElement(By.xpath("//select[@id='years']")).sendKeys("2002");
+		    // Address info 
+			//dr.findElement(By.xpath("//input[@id='firstname']")).sendKeys("akmal");
+			//dr.findElement(By.xpath("//input[@id='lastname']")).sendKeys("mabruka");
+			dr.findElement(By.xpath("//input[@id='company']")).sendKeys("Cybertek");
+			dr.findElement(By.xpath("//input[@id='address1']")).sendKeys("415 S Oxford ave");
+			dr.findElement(By.xpath("//input[@id='city']")).sendKeys("San Francisco");
+			dr.findElement(By.xpath("//select[@id='id_state']")).sendKeys("California");
+			dr.findElement(By.xpath("//input[@id='postcode']")).sendKeys("90020");
+			dr.findElement(By.xpath("//select[@id='id_country']")).sendKeys("USA");
+			dr.findElement(By.xpath("//input[@id='phone_mobile']")).sendKeys("444-555-8877");
+			dr.findElement(By.id("submitAccount")).click();
+			dr.findElement(By.xpath("//a[@class='logout']")).click();
+			// login back
+			
+			dr.findElement(By.xpath("//input[@id='email']")).sendKeys("akmal"+r+"@yahoo.com");
+			dr.findElement(By.xpath("//input[@name='passwd']")).sendKeys("12365478");
+			dr.findElement(By.id("SubmitLogin")).click();
+			
+			String expectedName="akmal mabruka";
+			String actualName=dr.findElement(By.xpath("(//a/span)[1]")).getText();
+			Assert.assertEquals(expectedName, actualName); 
+			
+			
+			
+			
+			
+			
+		}
+		
+
+	
+
+}
